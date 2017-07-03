3f42de233da8b80a261da101464daa8022da0ffb
luohongliang <369774822@qq.com>
2017-07-03 17:39:27 +0800

    generate ouyang dictionary in the right format

---
+++ Convert2simplified.py
@@ -0,0 +1,33 @@
+# -*- coding:UTF-8 -*-
+
+
+import os
+import os.path
+import string
+import sys
+import csv
+from hanziconv import HanziConv
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+
+count = 0
+
+a = '/home/hongliang/inpho/wordsegmentation/ouyang dictionary.txt'
+f = open(a,'r')
+fq = open('/home/hongliang/inpho/wordsegmentation/ouyang_dictionary.dic','w')
+
+for word in f.readlines():
+    word = word.strip().replace(u'\ufeff','')
+    word = word.replace('，','')
+    word = word.replace(' ','')
+    num = len(word)
+    sim_word = HanziConv.toSimplified(word)
+    sim_word = sim_word.encode('utf-8')
+    if num != 1:
+        content = str(num)+' '+sim_word+'\n'
+        fq.write(content)
+
+
+
+
